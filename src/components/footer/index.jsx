7cea23fc51f78b75e3aa282427441562c83a090b
SophieMalka <s.malka@live.fr>
2023-05-01 17:56:35 +0200

    footer ok

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,5 +1,13 @@
-function Footer() {
+import logo from '../../assets/pictures/logo-footer.png';
+import "./style.css";
 
+function Footer() {
+    return (
+        <footer className='footer'>
+            <img src={logo} className='logo-footer' alt='Logo Kasa' />
+            <p>© 2020 Kasa. All rights reserved</p>
+        </footer>
+    )
 };
 
 export default Footer
