42da7c5c865838e2dfc859e8f2fac7d71645deb6
SophieMalka <s.malka@live.fr>
2023-05-09 12:34:44 +0200

    Apartment title + location

--- src/pages/apartment/index.jsx
+++ src/pages/apartment/index.jsx
@@ -2,13 +2,25 @@
 import { useParams } from 'react-router-dom';
 import Slideshow from "../../components/slideshow";
 import datas from '../../assets/data/logements.json';
+import React from 'react';
+import './style.css'
 
 function Apartment() {
     const { id } = useParams();
     const data = datas.find(data => data.id === id);
+   
     return (
         <main>
             <Slideshow pictures={data.pictures} picturesLength={data.pictures.length} />
+            <article className='apartment-details'>
+                <div>
+                    <h2 className='apartment-title'>{data.title}</h2>
+                    <p className='apartment-location'>{data.location}</p>
+                    <div className='tag-container'>
+
+                    </div>
+                </div>
+            </article>
         </main>
     )
 };
