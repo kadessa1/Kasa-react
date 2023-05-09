636bdadf4568f84538cb0727096746ca2d5fa58b
SophieMalka <s.malka@live.fr>
2023-05-09 10:52:53 +0200

    Resolve error card() key

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,20 +1,23 @@
 import "./style.css";
 import datas from '../../assets/data/logements.json';
 import { Link } from 'react-router-dom';
+import React from "react";
 
 function Card() {
     return (
         <div className="cards">
-            {datas.map((data) => (
-                <Link to={`/apartment/${data.id}`}>
-                    <div key={data.id} className="card">
-                        <img src={data.cover} alt={data.title} />
-                        <h2>{data.title}</h2>
-                    </div>    
-                </Link>    
-            ))}
+            {React.Children.toArray(
+                datas.map((data) => (
+                    <Link to={`/apartment/${data.id}`}>
+                        <div className="card">
+                            <img src={data.cover} alt={data.title} />
+                            <h2>{data.title}</h2>
+                        </div>
+                    </Link>
+                ))
+            )}
         </div>
-    )
+    );
 };
 
 export default Card
