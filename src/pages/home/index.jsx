91c04c4b9256410d7dee42ef516564ff75d9278d
SophieMalka <s.malka@live.fr>
2023-05-21 18:03:08 +0200

    Ajustement css

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,5 +1,5 @@
-import Banner from "../../components/banner"
-import Card from "../../components/card"
+import Banner from "../../components/banner";
+import Card from "../../components/card";
 
 function Home() {
     return (
