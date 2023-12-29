45359a55454718f35a42140af55f1d076edeefe1
kadessa.dss <kadessa.dss@gmail.com>
2023-12-29 17:04:44 +0100

    version finale

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -3,7 +3,7 @@ import datas from '../../assets/data/logements.json';
 import { Link } from 'react-router-dom';
 import React from "react";
 
-function Card(props) {
+export default function Card(props) {
     if (props.page === 'home') {
         return (
             <div className="cards">
@@ -22,4 +22,55 @@ function Card(props) {
     }
 };
 
+/*
 export default Card
+
+import "./style.css";
+//import datas from '../../assets/data/logements.json';
+import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+
+function Card(props) {
+    /*
+    const [data, setData] = useState(null);
+    useEffect(() => {
+        fetch( "localhost:8080/api/properties", {
+            method: "GET",
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                setJoke(data[0].joke);
+                console.log(data);
+            })
+            .catch((error) => console.log(error));
+    }, []);
+
+
+    if (props.page === 'home') {
+        return (
+            <div className="cards">
+                {
+                    fetch("localhost:8080/api/properties",{ method: "GET" }).then(
+                        res => res.json().then(
+                            data => React.Children.toArray(data.map((data) => (
+                                <Link to={`/apartment/${data.id}`}>
+                                    <div className="card">
+                                        <img src={data.cover} alt={data.title} />
+                                        <h2>{data.title}</h2>
+                                    </div>
+                                </Link>
+                            ))
+                        )
+                    )
+                    //
+                    //   datas
+
+                )
+                }
+            </div>
+        )
+    }
+};
+
+export default Card
+*/
--- src/components/collapse/index.jsx
+++ src/components/collapse/index.jsx
@@ -1,28 +1,38 @@
-import styles from './style.module.css'
-import { useState } from "react"
-import arrow from '../../assets/pictures/arrow.png'
+import styles from "./style.module.css";
+import { useState } from "react";
+import arrow from "../../assets/pictures/arrow.png";
 
-function Collapse({collapseTitle , collapseDescription}){
-    const [isOpen, setIsOpen] = useState(false)
+function Collapse({ collapseTitle, collapseDescription }) {
+  const [isOpen, setIsOpen] = useState(false);
 
-    const handleClick = ()=>{
-        setIsOpen(!isOpen)
-    }
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
 
-    return(
-        <div className={styles.collapse_container}>
-            <div  onClick={handleClick} className={isOpen ?`${styles.collapse_title_open}`:`${styles.collapse_title_closed}` }>
-                {collapseTitle}
-                <img  src = {arrow} className={isOpen ?`${styles.arrow_up}`:`${styles.arrow_down}`}
-                alt='deployer' />
-            </div>
-            {isOpen && (
-                <div className={styles.collapse_description}>
-                    <div>{collapseDescription}</div>
-                </div>
-            )}
+  return (
+    <div className={styles.collapse_container}>
+      <div
+        onClick={handleClick}
+        className={
+          isOpen
+            ? `${styles.collapse_title_open}`
+            : `${styles.collapse_title_closed}`
+        }
+      >
+        {collapseTitle}
+        <img
+          src={arrow}
+          className={isOpen ? `${styles.arrow_down}` : `${styles.arrow_up}`}
+          alt="deployer"
+        />
+      </div>
+      {isOpen && (
+        <div className={styles.collapse_description}>
+          <div>{collapseDescription}</div>
         </div>
-    )
+      )}
+    </div>
+  );
 }
 
-export default Collapse
+export default Collapse;
